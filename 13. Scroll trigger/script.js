8ea5ad76089b6e6ae9244e5abf2ea902a251ea5a
Satyam173 <sm6604435@gmail.com>
2025-08-10 10:20:19 +0530

    13 scroll trigger

---
+++ 13. Scroll trigger/script.js
@@ -0,0 +1,11 @@
+gsap.from('.image-wrap-right',{
+    scrollTrigger : '.image-wrap-right',
+    x : 800,
+    delay : 0.7
+})
+
+gsap.from('.image-wrap-left',{
+    scrollTrigger : '.image-wrap-left',
+    x : -800,
+    delay : 0.7
+})
